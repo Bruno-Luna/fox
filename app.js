6d8de29fa63474b4fef6cb20362d7164ce8fb553
Bruno-Luna <luna.brsilva@gmail.com>
2023-04-17 18:38:35 -0300

    feat: função sortearImagem

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const img = document.querySelector('img')
+const button = document.querySelector('button')
 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://randomfox.ca/floof/");
-xhr.send();
-xhr.responseType = "json";
-xhr.onload = () => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    img.setAttribute('src', xhr.response.image)
-  } else {
-    console.error(`Error: ${xhr.status}`);
-  }
-};
+const sortearImagem = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://randomfox.ca/floof/");
+  xhr.send();
+  xhr.responseType = "json";
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      img.setAttribute('src', xhr.response.image)
+    } else {
+      console.error(`Error: ${xhr.status}`);
+    }
+  };
+}
+
+sortearImagem()
+
+button.addEventListener('click', sortearImagem)
+
 
