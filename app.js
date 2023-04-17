6189567db85e378b25a4e8070a7c73f8c9c27a88
Bruno-Luna <luna.brsilva@gmail.com>
2023-04-17 18:55:45 -0300

    refactor: espaços adicionados

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const button = document.querySelector('button')
 
 const sortearImagem = () => {
   const xhr = new XMLHttpRequest();
+
   xhr.open("GET", "https://randomfox.ca/floof/");
+
   xhr.send();
   xhr.responseType = "json";
+  
   xhr.onload = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       img.setAttribute('src', xhr.response.image)
