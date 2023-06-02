19eda1c4fbbcc6c4051da195efc95b18eb991045
Bruno-Luna <luna.brsilva@gmail.com>
2023-06-02 11:44:05 -0300

    refactor: nome da variavel

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,9 +2,9 @@ const img = document.querySelector('img')
 const button = document.querySelector('button')
 
 const raffleImage = () => {
-  const promisse = fetch('https://randomfox.ca/floof/')
+  const promise = fetch('https://randomfox.ca/floof/')
 
-  promisse.then(response => response.json())
+  promise.then(response => response.json())
              .then(data => img.setAttribute('src', data.image))
              .catch(err => console.error(err.message))
              .finally(console.info('Requisição finalizada!'))
